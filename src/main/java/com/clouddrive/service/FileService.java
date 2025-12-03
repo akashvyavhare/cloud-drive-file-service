a22fc813bc3f170b69e1a9ce3df31e227223f580
Akash <akash.vyavhare@veracity-india.com>
2025-12-03 20:43:10 +0530

    fileService methods added

--- src/main/java/com/clouddrive/service/FileService.java
+++ src/main/java/com/clouddrive/service/FileService.java
@@ -1,5 +1,14 @@
 package com.clouddrive.service;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.clouddrive.model.UserFile;
+
 public interface FileService {
 
+	UserFile uploadUserFile(String userID, MultipartFile file);
+	List<UserFile> listAllFiles(String userID);
+	void downloadFile(String userId, String fileId);
 }
